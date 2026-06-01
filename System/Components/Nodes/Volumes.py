c314c3f280efd261b61a40a59f3aec50b6a17417
Saaketh Ramoju <rsaaketh@icloud.com>
2026-05-31 20:00:51 -0400

    idk smth happened with volume, it's now changed back to normal

--- System/Components/Nodes/Volumes.py
+++ System/Components/Nodes/Volumes.py
@@ -26,7 +26,6 @@ class SimpleVolume(Component):
         mass_flow_out: State | None = None,
     ):
         self.setup()
-        self.composition.update()
 
     @property
     def iteration_variables(self) -> list[State]:
@@ -37,25 +36,20 @@ class SimpleVolume(Component):
         return [self.mass_flow_in.value - self.mass_flow_out.value]
 
 
-
-
 class IsothermalVolume(Component):
 
     def __init__(self,
-        name:str,
-        network: Network,
-        pressure: State,
-        temperature: State,
-        volume: float,
-        density: State | None = None,
-        enthalpy: State | None = None,
-        composition: Composition = Composition(),
-        composition_in: Composition = Composition(),
-        mass_flow_in: State | None = None,
-        mass_flow_out: State | None = None
-    ):
+                 name:str,
+                 network: Network,
+                 pressure: State,
+                 temperature: State,
+                 volume: float,
+                 density: State | None = None,
+                 enthalpy: State | None = None,
+                 mass_flow_in: State | None = None,
+                 mass_flow_out: State | None = None):
+        
         self.setup()
-        #self.composition.update()
     
     @property
     def iteration_variables(self) -> list[State]:
@@ -64,8 +58,6 @@ class IsothermalVolume(Component):
     @property
     def residuals(self) -> list[float]:
         return [self.mass_flow_in.value - self.mass_flow_out.value]
-
-
 
 
 
@@ -75,91 +67,36 @@ class Volume(Component):
         name: str,
         network: Network,
         pressure: State,
+        enthalpy: State,
         volume: float,
-        enthalpy: State | None = None,
+        total_enthalpy_in: State,
+        total_enthalpy_out: State | None = None,
+        heat_rate: State | float | None = None,
         temperature: State | None = None,
         density: State | None = None,
         internal_energy: State | None = None,
-        composition: Composition = Composition(),
-        composition_in: Composition = Composition(),
-        heat_rate: State | float | None = None,
-        total_enthalpy_in: State | None = None,
-        total_enthalpy_out: State | None = None,
         mass_flow_in: State | None = None,
         mass_flow_out: State | None = None,
     ):
         self.setup()
-        self.composition.update()
 
-        self._solve_energy = self.enthalpy.is_assigned and self.total_enthalpy_in.is_assigned
-
-        if self._solve_energy and not self.total_enthalpy_out.is_assigned:
+        if not self.total_enthalpy_out.is_assigned:
             self.total_enthalpy_out = self.enthalpy
 
     @property
     def iteration_variables(self) -> list[State]:
-        variables = [self.pressure]
-
-        if self._solve_energy:
-            variables.append(self.enthalpy)
-
-        return variables
+        return [self.pressure, self.enthalpy]
 
     @property
     def residuals(self) -> list[float]:
-        residuals = [
-            self.mass_flow_in.value - self.mass_flow_out.value
-        ]
-
-        if self._solve_energy:
-            qdot = self.heat_rate.value if self.heat_rate.is_assigned else 0.0
-
-            residuals.append(
-                self.mass_flow_in.value * self.total_enthalpy_in.value
-                - self.mass_flow_out.value * self.total_enthalpy_out.value
-                + qdot
-            )
-
-        return residuals
-    
-
-'''
-
-class SimpleVolume(Component):
-
-    def __init__(
-        self,
-        name: str,
-        network: Network,
-        pressure: State,
-        volume: float,
-        density: State | None = None,
-        temperature: State | None = None,
-        enthalpy: State | None = None,
-        composition: Composition = Composition(),
-        composition_in: Composition = Composition(),
-        mass_flow_in: State | None = None,
-        mass_flow_out: State | None = None,
-    ):
-        self.setup()
-        self.composition.update()
-
-    @property
-    def iteration_variables(self) -> list[State]:
-        return [
-            self.pressure,
-            *(self.composition[species] for species in self.composition & self.composition_in)
-        ]
-
-    @property
-    def residuals(self) -> list[float]:
-        self.composition.update()
+        qdot = self.heat_rate.value if self.heat_rate.is_assigned else 0.0
 
         return [
             self.mass_flow_in.value - self.mass_flow_out.value,
-            *(self.mass_flow_in.value * self.composition_in[species].value
-                - self.mass_flow_out.value * self.composition[species].value
-                for species in self.composition & self.composition_in
+            (
+                self.mass_flow_in.value * self.total_enthalpy_in.value
+                - self.mass_flow_out.value * self.total_enthalpy_out.value
+                + qdot
             ),
         ]
-'''
+    
--- test.py
+++ test.py
@@ -6,6 +6,118 @@ from constants import *
 
 MixtureNetwork = Network("Mixture Flow")
 
+# ------------------------------------------------------------------
+# Fluid properties
+# ------------------------------------------------------------------
+
+SourceFluid = IdealGasLookup(
+    "Source Fluid",
+    MixtureNetwork,
+    {"gn2": 0.75, "O2": 0.25},
+    pressure=3e5,
+    temperature=300,
+)
+
+VolumeFluid = FluidLookup(
+    "Volume Fluid",
+    MixtureNetwork,
+    SourceFluid.composition,
+    pressure=2e5,
+    temperature=300,
+)
+
+SeparatorOutlet1Fluid = FluidLookup(
+    "Separator Outlet 1 Fluid",
+    MixtureNetwork,
+    Composition("N2"),
+    pressure=VolumeFluid.pressure,
+    temperature=VolumeFluid.temperature,
+)
+
+SeparatorOutlet2Fluid = FluidLookup(
+    "Separator Outlet 2 Fluid",
+    MixtureNetwork,
+    Composition(),
+    pressure=VolumeFluid.pressure,
+    temperature=VolumeFluid.temperature,
+)
+
+# ------------------------------------------------------------------
+# Geometry
+# ------------------------------------------------------------------
+
+D = 3 * IN_TO_M
+A = (np.pi / 4) * D**2
+
+# ------------------------------------------------------------------
+# Components
+# ------------------------------------------------------------------
+
+Inlet = DischargeCoefficient(
+    "Inlet",
+    MixtureNetwork,
+    upstream_pressure=SourceFluid.pressure,
+    downstream_pressure=VolumeFluid.pressure,
+    density=SourceFluid.density,
+    discharge_coefficient=1,
+    cross_sectional_area=A,
+)
+
+Separator = FlowSplitter(
+    "Separator",
+    MixtureNetwork,
+    pressure=VolumeFluid.pressure,
+    volume=1,
+    mass_flow_in=Inlet.mass_flow,
+    composition=VolumeFluid.composition,
+    composition_out1=SeparatorOutlet1Fluid.composition,
+    composition_out2=SeparatorOutlet2Fluid.composition,
+)
+
+Outlet1 = DischargeCoefficient(
+    "Outlet 1",
+    MixtureNetwork,
+    upstream_pressure=Separator.pressure,
+    downstream_pressure=101325,
+    density=SeparatorOutlet1Fluid.density,
+    discharge_coefficient=1,
+    cross_sectional_area=A,
+    mass_flow=Separator.mass_flow_out1,
+)
+
+Outlet2 = DischargeCoefficient(
+    "Outlet 2",
+    MixtureNetwork,
+    upstream_pressure=Separator.pressure,
+    downstream_pressure=101325,
+    density=SeparatorOutlet2Fluid.density,
+    discharge_coefficient=1,
+    cross_sectional_area=A,
+    mass_flow=Separator.mass_flow_out2,
+)
+
+# ------------------------------------------------------------------
+# Solve
+# ------------------------------------------------------------------
+
+solution = SteadyState(MixtureNetwork).solve(
+    return_type="dataframe",
+    verbose=True,
+    static=False,
+    print_solution=True,
+    filename="test.xlsx",
+)
+
+
+
+'''from System import *
+from Solvers import *
+
+from constants import *
+
+
+MixtureNetwork = Network("Mixture Flow")
+
 SourceFluid = IdealGasLookup(
     "Source Fluid",
     MixtureNetwork,
@@ -89,4 +201,4 @@ solution = SteadyState(MixtureNetwork).solve(
     static=False,
     print_solution=True,
     filename="test.xlsx",
-)
+)'''
