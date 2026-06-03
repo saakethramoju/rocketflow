197f008f3a6ea1dc5b5db9b911270fe00bc210a5
Saaketh Ramoju <rsaaketh@icloud.com>
2026-06-03 01:46:20 -0400

    Pump model for model testing

--- ModelTest.py
+++ ModelTest.py
@@ -4,139 +4,222 @@ from Solvers import *
 from constants import *
 
 
-ModelNetwork = Network("Multiple Model Test")
+PumpNetwork = Network("Multi Model Pump Test")
 
 D = 3 * IN_TO_M
 A = (np.pi / 4) * D**2
 
-SourceFluid = FluidLookup(
-    "Source Fluid",
-    ModelNetwork,
+# ------------------------------------------------------------------
+# Fluids
+# ------------------------------------------------------------------
+
+InletFluid = FluidLookup(
+    "Inlet Fluid",
+    PumpNetwork,
     "rp-1",
     pressure=3e5,
     temperature=300,
 )
 
+OutletFluid = FluidLookup(
+    "Outlet Fluid",
+    PumpNetwork,
+    "rp-1",
+    pressure=9e5,
+    temperature=300,
+    flash_values=("pressure", "enthalpy"),
+)
+
 # ------------------------------------------------------------------
-# Model 1: Outlet 1
-# First option is intentionally bad.
+# Pump map setup
 # ------------------------------------------------------------------
 
-Outlet1MassFlow = State(1.0)
-Outlet1FrictionFactor = State(0.02)
-BadDensity = State(-1.0)
+RotorSpeed = State(22000.0)
+ImpellerDiameter = State(1.6 * IN_TO_M)
 
-Outlet1BadDarcy = ModelOption(
-    "bad_darcy",
+DesignCoefficients = TurboDesignCoefficients(
+    "Pump Design Coefficients",
+    PumpNetwork,
+    rotor_speed=22000.0,
+    volumetric_flow=2.5e-3,
+    head_rise=80.0,
+    torque=3.5,
+    density=InletFluid.density,
+    flow_geometric_parameter=ImpellerDiameter,
+    head_geometric_parameter=ImpellerDiameter,
+    torque_geometric_parameter=ImpellerDiameter,
+)
+
+normalized_flow_coefficient_map = [0.50, 0.75, 1.00, 1.25, 1.50]
+normalized_head_coefficient_map = [1.20, 1.10, 1.00, 0.80, 0.55]
+normalized_torque_coefficient_map = [0.60, 0.80, 1.00, 1.20, 1.45]
+
+# ------------------------------------------------------------------
+# Model 1: Pump model
+# ------------------------------------------------------------------
+
+ConstantPumpFlow = State(2.0e-3)
+ConstantPumpHeadRise = State()
+ConstantPumpTorque = State()
+
+ConstantDensityPumpOption = ModelOption(
+    "constant_density",
     components=[
-        Colebrook.model(
-            "Outlet 1 Bad Friction",
-            mass_flow=Outlet1MassFlow,
-            friction_factor=Outlet1FrictionFactor,
-            hydraulic_diameter=D,
-            dynamic_viscosity=SourceFluid.dynamic_viscosity,
-            cross_sectional_area=A / 4,
+        TurboMap.model(
+            "Constant Density Pump Map",
+            rotor_speed=RotorSpeed,
+            volumetric_flow=ConstantPumpFlow,
+            density=InletFluid.density,
+            flow_geometric_parameter=ImpellerDiameter,
+            head_geometric_parameter=ImpellerDiameter,
+            torque_geometric_parameter=ImpellerDiameter,
+            design_flow_coefficient=DesignCoefficients.flow_coefficient,
+            design_head_coefficient=DesignCoefficients.head_coefficient,
+            design_torque_coefficient=DesignCoefficients.torque_coefficient,
+            normalized_flow_coefficient_map=normalized_flow_coefficient_map,
+            normalized_head_coefficient_map=normalized_head_coefficient_map,
+            normalized_torque_coefficient_map=normalized_torque_coefficient_map,
+            head_rise=ConstantPumpHeadRise,
+            torque=ConstantPumpTorque,
         ),
-        DarcyWeisbach.model(
-            "Outlet 1 Bad Darcy",
-            mass_flow=Outlet1MassFlow,
-            upstream_pressure=SourceFluid.pressure,
-            downstream_pressure=101325,
-            length=1,
-            cross_sectional_area=A / 4,
-            hydraulic_diameter=D,
-            density=BadDensity,
-            friction_factor=Outlet1FrictionFactor,
+        ConstantDensityPump.model(
+            "Constant Density Pump",
+            rotor_speed=RotorSpeed,
+            head_rise=ConstantPumpHeadRise,
+            volumetric_flow=ConstantPumpFlow,
+            density=InletFluid.density,
+            torque=ConstantPumpTorque,
+            upstream_total_pressure=InletFluid.pressure,
+            discharge_total_pressure=OutletFluid.pressure,
+            upstream_total_enthalpy=InletFluid.enthalpy,
         ),
     ],
 )
 
-Outlet1CdA = DischargeCoefficient.model(
+PolyPumpMassFlow = State(1.5)
+PolyPumpHeadRise = State()
+PolyPumpTorque = State()
+
+PolytropicPumpOption = ModelOption(
+    "polytropic",
+    components=[
+        TurboMap.model(
+            "Polytropic Pump Map",
+            rotor_speed=RotorSpeed,
+            volumetric_flow=PolyPumpMassFlow / InletFluid.density,
+            density=InletFluid.density,
+            flow_geometric_parameter=ImpellerDiameter,
+            head_geometric_parameter=ImpellerDiameter,
+            torque_geometric_parameter=ImpellerDiameter,
+            design_flow_coefficient=DesignCoefficients.flow_coefficient,
+            design_head_coefficient=DesignCoefficients.head_coefficient,
+            design_torque_coefficient=DesignCoefficients.torque_coefficient,
+            normalized_flow_coefficient_map=normalized_flow_coefficient_map,
+            normalized_head_coefficient_map=normalized_head_coefficient_map,
+            normalized_torque_coefficient_map=normalized_torque_coefficient_map,
+            head_rise=PolyPumpHeadRise,
+            torque=PolyPumpTorque,
+        ),
+        PolytropicPump.model(
+            "Polytropic Pump",
+            rotor_speed=RotorSpeed,
+            head_rise=PolyPumpHeadRise,
+            mass_flow=PolyPumpMassFlow,
+            upstream_density=InletFluid.density,
+            downstream_density=OutletFluid.density,
+            torque=PolyPumpTorque,
+            upstream_total_pressure=InletFluid.pressure,
+            discharge_total_pressure=OutletFluid.pressure,
+            upstream_total_enthalpy=InletFluid.enthalpy,
+        ),
+    ],
+)
+
+PumpModel = Model(
+    "Main Pump",
+    PumpNetwork,
+    ConstantDensityPumpOption,
+    PolytropicPumpOption,
+    order=[
+        "constant_density",
+        "polytropic",
+    ],
+)
+
+# ------------------------------------------------------------------
+# Model 2: Outlet model
+# ------------------------------------------------------------------
+
+OutletCdA = DischargeCoefficient.model(
     "cda",
-    upstream_pressure=SourceFluid.pressure,
-    downstream_pressure=101325,
-    density=SourceFluid.density,
-    discharge_coefficient=1,
+    upstream_pressure=OutletFluid.pressure,
+    downstream_pressure=101325.0,
+    density=OutletFluid.density,
+    discharge_coefficient=1.0,
     cross_sectional_area=A / 4,
 )
 
-Outlet1 = Model(
-    "Outlet 1",
-    ModelNetwork,
-    Outlet1BadDarcy,
-    Outlet1CdA,
-    order=[
-        "bad_darcy",
-        "cda",
-    ],
-)
+OutletMassFlow = State(1.0)
+OutletFrictionFactor = State(0.02)
 
-# ------------------------------------------------------------------
-# Model 2: Outlet 2
-# Multiple valid options.
-# Only the first/default option should be used during Outlet 1 sweep.
-# ------------------------------------------------------------------
-
-Outlet2MassFlow = State(1.0)
-Outlet2FrictionFactor = State(0.02)
-
-Outlet2Darcy = ModelOption(
+OutletDarcy = ModelOption(
     "darcy",
     components=[
         Churchill.model(
-            "Outlet 2 Friction",
-            mass_flow=Outlet2MassFlow,
-            friction_factor=Outlet2FrictionFactor,
+            "Outlet Friction",
+            mass_flow=OutletMassFlow,
+            friction_factor=OutletFrictionFactor,
             hydraulic_diameter=D,
-            dynamic_viscosity=SourceFluid.dynamic_viscosity,
-            cross_sectional_area=A,
+            dynamic_viscosity=OutletFluid.dynamic_viscosity,
+            cross_sectional_area=A / 4,
         ),
         DarcyWeisbach.model(
-            "Outlet 2 Darcy",
-            mass_flow=Outlet2MassFlow,
-            upstream_pressure=SourceFluid.pressure,
-            downstream_pressure=101325,
-            length=1,
-            cross_sectional_area=A,
+            "Outlet Darcy",
+            mass_flow=OutletMassFlow,
+            upstream_pressure=OutletFluid.pressure,
+            downstream_pressure=101325.0,
+            length=1.0,
+            cross_sectional_area=A / 4,
             hydraulic_diameter=D,
-            density=SourceFluid.density,
-            friction_factor=Outlet2FrictionFactor,
+            density=OutletFluid.density,
+            friction_factor=OutletFrictionFactor,
         ),
     ],
 )
 
-Outlet2CdA = DischargeCoefficient.model(
-    "cda",
-    upstream_pressure=SourceFluid.pressure,
-    downstream_pressure=101325,
-    density=SourceFluid.density,
-    discharge_coefficient=1,
-    cross_sectional_area=A,
-)
-
-Outlet2 = Model(
-    "Outlet 2",
-    ModelNetwork,
-    Outlet2Darcy,
-    Outlet2CdA,
+OutletModel = Model(
+    "Outlet",
+    PumpNetwork,
+    OutletCdA,
+    OutletDarcy,
     order=[
-        "darcy",
         "cda",
+        "darcy",
     ],
 )
 
 # ------------------------------------------------------------------
-# Sweep Outlet 1.
-# Expected:
-#   - Outlet 1 bad_darcy fails and is skipped.
-#   - Outlet 1 cda succeeds.
-#   - Outlet 2 has multiple options, but only its default darcy option is used.
+# Test A: normal solve
+# Uses first option from each model:
+#   Main Pump -> constant_density
+#   Outlet    -> cda
+# ------------------------------------------------------------------
+'''
+solution = SteadyState(PumpNetwork).solve(
+    verbose=True,
+    print_solution=True,
+    filename="multi_model_default.xlsx",
+)
+'''
+# ------------------------------------------------------------------
+# Test B: sweep only the pump model
+# Uncomment to test all Main Pump options.
 # ------------------------------------------------------------------
 
-results = SteadyState(ModelNetwork).solve(
-    model="Outlet 1",
-    evaluate_all_model_options=True,
+SteadyState(PumpNetwork).solve(
+    model="Main Pump",
     verbose=True,
     print_solution=True,
     filename="test.xlsx",
+    evaluate_all_model_options=True
 )
